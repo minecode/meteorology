9ff19edcda0418250b8fd953fb6b38e553502864
Fábio Henriques <fabiomchenriques@ua.pt>
2020-04-23 22:08:22 +0100

    Updates

---
+++ precache-manifest.63bb62c88ada9530c5da1577d881178d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322170855bfb17236d1a900fd88da94a",
+    "url": "/meteorology/index.html"
+  },
+  {
+    "revision": "e85ba3fd77e06ccd5b52",
+    "url": "/meteorology/static/css/2.55b6c765.chunk.css"
+  },
+  {
+    "revision": "873c369efb7594c1eeda",
+    "url": "/meteorology/static/css/main.4ab05b0f.chunk.css"
+  },
+  {
+    "revision": "e85ba3fd77e06ccd5b52",
+    "url": "/meteorology/static/js/2.18f42e36.chunk.js"
+  },
+  {
+    "revision": "b3e1faca98a274fcbdaf39c9637fb7d8",
+    "url": "/meteorology/static/js/2.18f42e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873c369efb7594c1eeda",
+    "url": "/meteorology/static/js/main.52f8315b.chunk.js"
+  },
+  {
+    "revision": "21248c11a32c8ec01f1f",
+    "url": "/meteorology/static/js/runtime-main.f94387be.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/meteorology/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/meteorology/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/meteorology/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/meteorology/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/meteorology/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/meteorology/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/meteorology/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/meteorology/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/meteorology/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/meteorology/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/meteorology/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c9e3561c620d4a725d69fcb23eca79e4",
+    "url": "/meteorology/static/media/logo.c9e3561c.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/meteorology/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/meteorology/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/meteorology/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/meteorology/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/meteorology/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
